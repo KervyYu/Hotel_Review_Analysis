3db8660a117ffcfb540b4d6ef71b998c2bf8983a
Kervy Yu <kerbs.yu@gmail.com>
2025-07-19 15:42:49 +0800

    practice

--- analysis/test.sql
+++ analysis/test.sql
@@ -1,20 +1,5 @@
 SELECT
-    hotel_name AS hotel,
-   COUNT(score_group) AS rating
-FROM hotel_reviews
-WHERE
-    score_group = 'Negative'
-GROUP BY hotel_name
-ORDER BY rating ASC;
-
-
-
-
-SELECT
-    hotel_name,
-    COUNT(score_group) AS rating,
-   RANK() OVER( ORDER BY COUNT(score_group) DESC) AS ranking
-FROM hotel_reviews
-WHERE
-    score_group = 'Negative'
-GROUP BY hotel_name
+ DISTINCT hotel_name
+FROM
+    hotel_reviews
+Limit 10
